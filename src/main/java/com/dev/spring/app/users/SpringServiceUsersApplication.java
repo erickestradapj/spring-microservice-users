f2b3ca943996c065773d60d075704e2ff022da82
Erick Estrada <erickestradapj@gmail.com>
2021-11-08 11:55:50 -0500

    add entity

---
+++ src/main/java/com/dev/spring/app/users/SpringServiceUsersApplication.java
@@ -0,0 +1,13 @@
+package com.dev.spring.app.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServiceUsersApplication.class, args);
+	}
+
+}
